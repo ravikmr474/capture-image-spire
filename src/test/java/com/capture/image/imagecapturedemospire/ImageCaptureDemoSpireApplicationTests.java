be1106df51004835a97d55513e892b81dc0d8140
ravikmr474 <40664321+ravikmr474@users.noreply.github.com>
2023-03-17 16:48:49 +0530

    initial commit

---
+++ src/test/java/com/capture/image/imagecapturedemospire/ImageCaptureDemoSpireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capture.image.imagecapturedemospire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageCaptureDemoSpireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
